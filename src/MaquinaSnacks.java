1ad7c868a2e2b14ce818ddbd2544a9cf9d14004c
s4archivosxx <jordan_vasquez_2019@hotmail.com>
2025-04-27 17:32:05 -0500

    final de la aplicación

--- src/MaquinaSnacks.java
+++ src/MaquinaSnacks.java
@@ -21,7 +21,7 @@ public class MaquinaSnacks {
         while (!salir){
             try {
                 var opcion = mostrarMenu(consola);
-                // salir = ejecutarOpciones(opcion, consola, productos);
+                salir = ejecutarOpciones(opcion, consola, productos);
             }catch (Exception e){
                 System.out.println("Ocurrio un error: " + e.getMessage());
             }
@@ -45,19 +45,58 @@ public class MaquinaSnacks {
         return Integer.parseInt(consola.nextLine());
     }
 
-    private static void ejecutarOpciones(){
-
+    private static boolean ejecutarOpciones(int opcion, Scanner consola, List<Snack> productos){
+        var salir = false;
+        switch (opcion){
+            case 1 -> comprarSnacks(consola, productos);
+            case 2 -> mostrarTicket(productos);
+            case 3 -> agregarSnack(consola);
+            case 4 -> {
+                System.out.println("\nRegresa pronto!");
+                salir = true;
+            }
+            default -> System.out.println("\nOpción inválida: " + opcion);
+        }
+        return salir;
     }
 
-    private static void comprarSnacks(){
+    private static void comprarSnacks(Scanner consola, List<Snack> productos){
+        System.out.print("¿Que snack quieres comprar (id)? ");
+        var idSnack = Integer.parseInt(consola.nextLine());
+        // Validar que el snack exista en la lista de snacks
+        var snackEncontrado = false;
+        for(var snack: SnackManager.getSnacks()){
+            if (snack.getIdSnack() == idSnack){
+                productos.add(snack);
+                System.out.println("Ok, Snack agregado: " + snack);
+                snackEncontrado = true;
+                break;
+            }
+        }
 
+        if (!snackEncontrado){
+            System.out.println("Id de snack no encontrado: " + idSnack);
+        }
     }
 
-    private static void mostrarTicket(){
-
+    private static void mostrarTicket(List<Snack> productos){
+        var ticket = "\n*** Ticket de Venta ***";
+        var total = 0.0;
+        for(var producto: productos){
+            ticket += "\n\t-" + producto.getNombre() + " - S/" + producto.getPrecio();
+            total += producto.getPrecio();
+        }
+        ticket +="\n\tTotal -> S/" + total;
+        System.out.println(ticket);
     }
 
-    private  static void agregarSnack(){
-
+    private  static void agregarSnack(Scanner consola){
+        System.out.print("Nombre del snack: ");
+        var nombre = consola.nextLine();
+        System.out.print("Precio del snack: ");
+        var precio = Double.parseDouble(consola.nextLine());
+        SnackManager.agregarSnack(new Snack(nombre, precio));
+        System.out.println("Tu snack se ha agregado correctamente");
+        SnackManager.mostrarSnacks();
     }
 }
